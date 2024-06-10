25df05f3ba93c006fd88e5ef0ed8eadc0b6a1e5e
Elian <e.bustamante02@ufromail.cl>
2024-06-09 20:27:58 -0400

    fix: Correccion de constante API_WILDCARE

--- src/consts/APIWildCare.ts
+++ src/consts/APIWildCare.ts
@@ -1,2 +1,2 @@
-// export const API_WILDCARE = 'https://wildcare-api-production.up.railway.app'
-export const API_WILDCARE = 'http://localhost:3000'
+export const API_WILDCARE = 'https://wildcare-api-production.up.railway.app'
+//export const API_WILDCARE = 'http://localhost:3000'
