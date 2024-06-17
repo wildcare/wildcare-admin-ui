90a33deda519ee19be00cf55773d03cd5d04cf7c
Matias Paillal <m.paillal01@ufromail.cl>
2024-06-16 22:42:04 -0400

    feat: se añaden estilos a botones del sidebar

--- src/assets/icons/Add.tsx
+++ src/assets/icons/Add.tsx
@@ -1,19 +1,22 @@
-const Add: React.FC = () => {
-    return (
-        <svg
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                id="Vector"
-                d="M9 13.7895V4.78955M4.5 9.28955H13.5M9 16.7895C13.1421 16.7895 16.5 13.4317 16.5 9.28955C16.5 5.14741 13.1421 1.78955 9 1.78955C4.85786 1.78955 1.5 5.14741 1.5 9.28955C1.5 13.4317 4.85786 16.7895 9 16.7895Z"
-                stroke="white"
-                strokeWidth="2" />
-        </svg>
-    )
+import { PropsIcon } from '../../types'
+
+const Add: React.FC<PropsIcon> = ({ activo }) => {
+	return (
+		<svg
+			width="18"
+			height="18"
+			viewBox="0 0 18 18"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				id="Vector"
+				d="M9 13.7895V4.78955M4.5 9.28955H13.5M9 16.7895C13.1421 16.7895 16.5 13.4317 16.5 9.28955C16.5 5.14741 13.1421 1.78955 9 1.78955C4.85786 1.78955 1.5 5.14741 1.5 9.28955C1.5 13.4317 4.85786 16.7895 9 16.7895Z"
+				stroke={activo ? '#17c964' : 'white'}
+				strokeWidth="2"
+			/>
+		</svg>
+	)
 }
 
 export default Add
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,10 +6,13 @@ import Menu from '../../assets/icons/Menu'
 import Add from '../../assets/icons/Add'
 import UploadFile from '../../assets/icons/UploadFile'
 import { useAuth } from '../../../context/auth/useAuth'
+import Logout from '../../assets/icons/Logout'
 
 const Sidebar: React.FC = () => {
 	const [estaAbierto, setEstadoSidebar] = useState(false)
-	const [botonActivo, setBotonActivo] = useState('')
+	const [botonActivo, setBotonActivo] = useState(
+		localStorage.getItem('botonActivo') || 'Especies'
+	)
 
 	const { cerrarSesion } = useAuth()
 	useEffect(() => {
@@ -28,18 +31,17 @@ const Sidebar: React.FC = () => {
 		estaAbierto ? 'w-[15%] min-w-[160px]' : 'w-[65px] min-w-[65px]'
 	}`
 
-	const clasesLogo = `flex mt-14 gap-1 md:text-[18px]   ${
+	const clasesLogo = `flex mt-14 ml-1 gap-1 md:text-[18px]   ${
 		estaAbierto
 			? 'transition-all duration-1000 text-white'
 			: 'text-transparent transition-all duration-300'
 	}`
-
 	return (
 		<div className={clasesContSidebar}>
 			<div className="h-[80%]">
-				<div className="z-30 top-4  flex absolute">
+				<div className="z-30 top-4 left-5 flex absolute">
 					<button
-						className="text-white hover:bg-white/20 rounded-full  transition duration-300"
+						className="text-white hover:bg-white/20 rounded-sm  transition duration-300"
 						onClick={alternarEstadoSidebar}
 					>
 						<Menu />
@@ -47,7 +49,7 @@ const Sidebar: React.FC = () => {
 				</div>
 
 				<div id="logo" className={clasesLogo}>
-					<h1 className="text-[19px] font-semibold">WildCare</h1>
+					<h1 className="text-[18px] font-semibold">WildCare</h1>
 					<div>
 						<span className="text-xs font-semibold italic uppercase">A</span>
 						<span className="text-xs font-semibold italic lowercase">dmin</span>
@@ -59,28 +61,28 @@ const Sidebar: React.FC = () => {
 					className="mt-10 w-full flex flex-col justify-center items-center gap-2"
 				>
 					<BotonSidebar
-						icono={<Deer />}
+						icono={<Deer activo={botonActivo === 'Especies'} />}
 						texto="Especies"
 						url="/home"
 						estaAbierto={estaAbierto}
 						cambiarBotonActivo={setBotonActivo}
 					/>
 					<BotonSidebar
-						icono={<Binoculars />}
+						icono={<Binoculars activo={botonActivo === 'Avistamientos'} />}
 						texto="Avistamientos"
 						url="/avistamientos"
 						estaAbierto={estaAbierto}
 						cambiarBotonActivo={setBotonActivo}
 					/>
 					<BotonSidebar
-						icono={<Add />}
+						icono={<Add activo={botonActivo === 'Nuevo'} />}
 						texto="Nuevo"
 						url="/registrar-especimen"
 						estaAbierto={estaAbierto}
 						cambiarBotonActivo={setBotonActivo}
 					/>
 					<BotonSidebar
-						icono={<UploadFile />}
+						icono={<UploadFile activo={botonActivo === 'Cargar archivo'} />}
 						texto="Cargar archivo"
 						url="/cargar-archivo"
 						estaAbierto={estaAbierto}
@@ -91,7 +93,7 @@ const Sidebar: React.FC = () => {
 
 			<div className="w-full h-[10%] flex items-end">
 				<BotonSidebar
-					icono={<Deer />}
+					icono={<Logout />}
 					texto="Cerrar sesion"
 					url="/login"
 					estaAbierto={estaAbierto}
